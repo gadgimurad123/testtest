050cd1c219beb74a0028c12470e9e25b450f5557
Валерий <dmitrevsky.vv@mnpk.lan>
2021-03-05 10:20:51 +0300

    add new string and edit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World! Editing this message");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        System.out.println("Hello World! From main branch");
     }
 }
